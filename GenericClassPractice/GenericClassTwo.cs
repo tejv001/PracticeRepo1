1028e86bb562c036d3ef898f7611a9a2e8c791a2
tvemulapalli <tvemulapalli@suguro-soft.com>
2025-07-23 19:45:41 +0530

    committing only one file

--- GenericClassPractice/GenericClassTwo.cs
+++ GenericClassPractice/GenericClassTwo.cs
@@ -2,9 +2,6 @@
 
 public class GenericClassTwo<T>
 {
-<<<<<<< Updated upstream
-	public T Field1;//comment n
-=======
 	public T Field1;//comment n added by teja xfdgnxdg added
->>>>>>> Stashed changes
+	public T Field1;//comment n
 }
